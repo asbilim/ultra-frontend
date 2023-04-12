2d40be33b235ce815dd62b4bb59e775cb9238a39
asbilim <bilimdepaullilian@gmail.com>
2023-04-13 00:21:51 +0100

    another thing done

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,27 +1,46 @@
+'use client'
+
+import { useForm } from "react-hook-form"
+
 export default function Login(){
+
+    const {register,handleSubmit,formState:{errors}} = useForm()
+
+    const onsubmit = data =>console.log(data)
+
     return(
         <div className="hero min-h-screen bg-base-200">
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 ">
-        <div className="card-body">
+        <form className="card-body" onSubmit={handleSubmit(onsubmit)}>
             <div className="form-control">
-            <label className="label">
-                <span className="label-text">Email</span>
+                <label className="label">
+                    <span className="label-text">Email</span>
+                </label>
+                <input type="text" placeholder="email" className="input input-bordered" {...register("email",{required:"please enter your email address",pattern:{value:"/^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/",message:"please enter a valid email"}})} />
+                <label className="label">
+                <p className="text-error">{errors.email?.message}</p>
             </label>
-            <input type="text" placeholder="email" className="input input-bordered" />
             </div>
             <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Password</span>
+                </label>
+                <input type="password" className="input input-bordered" {...register("password",{required:"please enter a valid password"})}/>
             <label className="label">
-                <span className="label-text">Password</span>
-            </label>
-            <input type="text" placeholder="password" className="input input-bordered" />
-            <label className="label">
-                <a href="/auth/register" className="label-text-alt link link-hover font-bold">Register instead</a>
+                <p className="text-error">{errors.password?.message}</p>
             </label>
             </div>
+            <a href="/auth/register" className="label-text-alt link link-hover font-bold text-lg underline">Register instead</a>
             <div className="form-control mt-6">
-            <button className="btn btn-primary">Login</button>
+                {
+                    errors.email || errors.password ? (
+                        <button type="submit" className="btn btn-primary" disabled>Login</button>
+                    ):(
+                        <button type="submit" className="btn btn-primary">Login</button>
+                    )
+                }
             </div>
-        </div>
+        </form>
         </div>
     </div>
     )
--- src/app/auth/register/page.js
+++ src/app/auth/register/page.js
@@ -1,39 +1,67 @@
-export default function Login(){
+"use client"
+
+import { useForm } from "react-hook-form"
+
+export default function Register(){
+
+    const {register,handleSubmit,formState:{errors},watch} = useForm() 
+    onsubmit = data => console.log(data)
+
     return(
         <div className="hero min-h-screen bg-base-200">
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 ">
-        <div className="card-body">
+        <form className="card-body" onSubmit={handleSubmit(onsubmit)}>
             <div className="form-control">
                 <label className="label">
                     <span className="label-text">Email</span>
                 </label>
-                <input type="text" placeholder="email" className="input input-bordered" />
+                <input type="text" placeholder="email" className="input input-bordered" {...register("email",{required:"please enter your email address",pattern:{value:"/^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/",message:"please enter a valid email"}})} />
+                <label className="label">
+                    <p className="text-error">{errors.email?.message}</p>
+                </label>
             </div>
             <div className="form-control">
                 <label className="label">
-                    <span className="label-text">Username</span>
+                    <span className="label-text" >Username</span>
                 </label>
-                <input type="text" placeholder="username" className="input input-bordered" />
+                <input type="text" placeholder="username" className="input input-bordered" {...register('username',{required:"please enter a valid username",minLength:{value:6,message:"Your username is too short"}})}/>
+                <label className="label">
+                    <p className="text-error">{errors.username?.message}</p>
+                </label>
             </div>
             <div className="form-control">
                 <label className="label">
                     <span className="label-text">Password</span>
                 </label>
-                <input type="text" placeholder="#v3ryStr0n5pa55" className="input input-bordered" />
+                <input type="password" className="input input-bordered" {...register("password",{required:"please enter a valid password",pattern:"/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/",message:"your password is not strong enough"})}/>
+                <label className="label">
+                    <p className="text-error">{errors.password?.message}</p>
+                </label>
             </div>
             <div className="form-control">
                 <label className="label">
                     <span className="label-text">Confirm</span>
                 </label>
-                <input type="text" placeholder="confirm password" className="input input-bordered" />
+                <input type="password" placeholder="confirm password" {...register("confirm",{required:"please confirm your password",validate:(confirm)=>{
+                    if(watch("password") !== confirm){
+                        return "the two password are not same"
+                    }
+                }})} className="input input-bordered" />
                 <label className="label">
-                    <a href="#" className="label-text-alt link link-hover">Login instead</a>
+                    <p className="text-error">{errors.confirm?.message}</p>
                 </label>
             </div>
+            <a href="/auth/login" className="label-text-alt link link-hover text-lg underline">Login instead</a>
             <div className="form-control mt-6">
-            <button className="btn btn-primary">Create an account</button>
+                {
+                    errors.username || errors.email || errors.password || errors.confirm ? (
+                        <button className="btn btn-primary" disabled>Create an account</button>
+                    ):(
+                        <button className="btn btn-primary">Create an account</button>
+                    )
+                }
             </div>
-        </div>
+        </form>
         </div>
     </div>
     )
